b3e1380c7aad6cc6b014d823cdec16ba73542ab0
Will Wilkinson <40211746+wfw40869@users.noreply.github.com>
2019-07-15 10:30:49 -0500

    Initial Commit. Project Completed

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+for (let i = 1; i <= 100; i++) {
+    if (i % 5 === 0 & i % 7 == 0) {
+        console.log("ChickenMonkey")
+        continue
+    } else if (i % 5 === 0) {
+        console.log("Chicken");
+    } else if (i % 7 == 0) {
+        console.log("Monkey")
+    }
+    else {
+        console.log(i)
+    }
+
+}
